8a44e981e181025d313a90ff5a49dd9fa6e08a97
May Wang <wxm112@me.com>
2014-12-23 16:44:16 +1100

    Bug cannot be found

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var CountryViewModel = function(name) {
+  this.name = ko.observable(name);
+};
 
 var ViewModel = function(){
   var self = this;
@@ -6,5 +9,21 @@ var ViewModel = function(){
   self.fullname = ko.computed(function(){
     return self.firstname() + " " + self.lastname();
   });
+  self.countries = ko.observableArray([
+    new CountryViewModel("China"),
+    new CountryViewModel("Sweden"),
+    new CountryViewModel("Australia")]);
 };
+
+var ListModel = function (items) {
+    this.items = ko.observableArray([items]);
+    this.itemToAdd = ko.observable("");
+    this.addItem = function () {
+        if (this.itemToAdd() != "") { 
+            this.items.push(this.itemToAdd());
+            this.itemToAdd(""); // Clear the text box
+    };
+  }.bind(this);
+};
+
 
