e7e10255692f20a3ae8114b7e03ce32a2c090df4
May Wang <wxm112@me.com>
2014-12-23 17:43:22 +1100

    Fixed, using the 猪

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ var CountryViewModel = function(name) {
   this.name = ko.observable(name);
 };
 
-var ViewModel = function(){
+var ViewModel = function() {
   var self = this;
   self.firstname = ko.observable("May");
   self.lastname = ko.observable("Wang");
-  self.fullname = ko.computed(function(){
+  self.fullname = ko.computed(function() {
     return self.firstname() + " " + self.lastname();
   });
   self.countries = ko.observableArray([
     new CountryViewModel("China"),
     new CountryViewModel("Sweden"),
-    new CountryViewModel("Australia")]);
+    new CountryViewModel("Australia")
+  ]);
 };
 
-var ListModel = function (items) {
-    this.items = ko.observableArray([items]);
-    this.itemToAdd = ko.observable("");
-    this.addItem = function () {
-        if (this.itemToAdd() != "") { 
-            this.items.push(this.itemToAdd());
-            this.itemToAdd(""); // Clear the text box
-    };
-  }.bind(this);
-};
-
-
+var ListModel = function(items) {
+  var self = this;
+  self.items = ko.observableArray(items);
+  self.itemToAdd = ko.observable("");
+  self.addItem = function() {
+    if (self.itemToAdd() != "") {
+      self.items.push(self.itemToAdd());
+      self.itemToAdd(""); // Clear the text box
+    }
+  };
+}
